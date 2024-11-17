e9e4361f09aba35013afbd69f2fc26d4a3115007
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-11-17 17:20:13 +0100

    01 laravel-bootcamp-chirpers-blade: render using Blade

--- laravel-chirpers-blade/app/Http/Controllers/ChirpController.php
+++ laravel-chirpers-blade/app/Http/Controllers/ChirpController.php
@@ -5,16 +5,18 @@ namespace App\Http\Controllers;
 use App\Models\Chirp;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\View\View;
 
 class ChirpController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(): Response
+    // public function index(): Response
+    public function index(): View
     {
-        //
-        return response('Hello, world !');
+        // return response('Hello, world !');
+        return view('chirps.index');
     }
 
     /**
